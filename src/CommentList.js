d83134755764311e7452edb4ee457a03f5280e0d
zaki-yama <shingoyamazaki00@gmail.com>
2016-03-21 06:23:10 +0900

    add Comment

--- src/CommentList.js
+++ src/CommentList.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Comment from './Comment';
 
 export default class CommentList extends React.Component {
   render() {
     return (
       <div className="commentList">
-        Hello, world! I am a CommentList.
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
       </div>
     );
   }
