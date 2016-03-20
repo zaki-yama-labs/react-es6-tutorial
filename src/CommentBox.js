ca1c0a132779db3fa1a43b872366300265a23d4c
zaki-yama <shingoyamazaki00@gmail.com>
2016-03-21 05:56:29 +0900

    divide app.js and module(CommentBox)

---
+++ src/CommentBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class CommentBox extends React.Component {
+  render() {
+    return (
+      <div className="commentBox">
+        Hello, world I am a CommentBox.
+      </div>
+    );
+  }
+}
--- src/app.js
+++ src/app.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-class CommentBox extends React.Component {
-  render() {
-    return (
-      <div className="commentBox">
-        Hello, world I am a CommentBox.
-      </div>
-    );
-  }
-}
+import CommentBox from './CommentBox';
 
 ReactDOM.render(
   <CommentBox />,
