b8373a7a741d83146348c1c9efe51a46eab26455
zaki-yama <shingoyamazaki00@gmail.com>
2016-03-21 06:32:10 +0900

    use markdown

--- src/Comment.js
+++ src/Comment.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import marked from 'marked';
 
 export default class Comment extends React.Component {
+  rawMarkup() {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    return {__html: rawMarkup};
+  }
+
   render() {
     return (
       <div className="comment">
         <h2 className="commentAuthor">
           {this.props.author}
         </h2>
-        {this.props.children}
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
       </div>
     );
   }
